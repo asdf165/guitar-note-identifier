c26b2257eef11cb7f3d0e4d9536dcf02890e5d26
asdf165 <githubsample85@gmail.com>
2026-03-19 21:39:27 -0700

    Add config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+STANDARD_TUNING_MIDI = {
+    6: 40,  # E2
+    5: 45,  # A2
+    4: 50,  # D3
+    3: 55,  # G3
+    2: 59,  # B3
+    1: 64,  # E4
+}
+
+NOTE_NAMES_SHARP = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
